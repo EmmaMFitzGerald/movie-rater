2cb2521cc688a0b2d85b903b1e24b5bc90e89580
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-08-07 15:53:54 +0000

    first commit

---
+++ lib/movie/rater.rb
@@ -0,0 +1,8 @@
+require "movie/rater/version"
+
+module Movie
+  module Rater
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/movie/rater/version.rb
@@ -0,0 +1,5 @@
+module Movie
+  module Rater
+    VERSION = "0.1.0"
+  end
+end
